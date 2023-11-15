d8ff4c3a7f69234ca8b91201091ae359cbea05be
Mat Sz <contact@matsz.dev>
2023-11-15 19:03:00 +0100

    add preview for multiple output images

--- web/src/views/project/Preview.tsx
+++ web/src/views/project/Preview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import styles from './Preview.module.scss';
@@ -6,12 +6,31 @@ import { mainStore } from '../../stores/MainStore';
 
 export const Preview: React.FC = observer(() => {
   const project = mainStore.project!;
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const selected = project.outputFilenames[selectedIndex];
+  const path =
+    selected && project.view('output', project.outputFilenames[selectedIndex]);
+
+  useEffect(() => {
+    setSelectedIndex(0);
+  }, [project.outputFilenames, setSelectedIndex]);
 
   return (
     <div className={styles.preview}>
-      {project.outputFilenames.map(filename => (
-        <img src={project.view('output', filename)} key={filename} />
-      ))}
+      <div className={styles.image}>{path && <img src={path} />}</div>
+      {project.outputFilenames.length > 1 && (
+        <div className={styles.thumbnails}>
+          {project.outputFilenames.map((filename, i) => (
+            <img
+              className={selectedIndex === i ? styles.selected : undefined}
+              src={project.view('output', filename)}
+              key={i}
+              onClick={() => setSelectedIndex(i)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 });
