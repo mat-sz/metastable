7a2593540b53eaf8f43f0640911f6cb17f9bf121
Mat Sz <contact@matsz.dev>
2023-11-20 18:25:21 +0100

    add database schema

---
+++ api/database.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Project(Base):
+    __tablename__ = 'projects'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    settings = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self):
+        return f'Project {self.name}'
+
+class Model(Base):
+    __tablename__ = 'models'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    model_type = Column(String)
+    filename = Column(String)
+    source = Column(String)
+    source_id = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self):
+        return f'Model {self.name}'
+
+Base.metadata.create_all(engine)
