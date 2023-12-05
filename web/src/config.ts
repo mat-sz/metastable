3e4b247aaff6fd82ad9f751c03730664c2933137
Mat Sz <contact@matsz.dev>
2023-12-05 14:11:40 +0100

    use current host

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,5 +1,5 @@
 export const SERVER_HOST =
-  import.meta.env.VITE_APP_SERVER_HOST || 'localhost:5000';
+  import.meta.env.VITE_APP_SERVER_HOST || window.location.host;
 
 export function getUrl(path: string, protocol = 'http') {
   return `${protocol}://${SERVER_HOST}${path}`;
