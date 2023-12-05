6f8223546b03e6ca6be2b1882fd70725ea688bc5
Mat Sz <contact@matsz.dev>
2023-12-05 14:09:26 +0100

    fix default host

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: '0.0.0.0',
     port: 3000,
   },
   build: {
