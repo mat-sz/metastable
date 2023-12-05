35cf22a5c10fb056a011e28abc1e5102b4d35438
Mat Sz <contact@matsz.dev>
2023-12-05 14:32:48 +0100

    improve handling of incomplete downloads

--- api/src/downloader.ts
+++ api/src/downloader.ts
@@ -1,8 +1,11 @@
 import path from 'path';
-import fs from 'fs';
+import fs from 'fs/promises';
+import { createWriteStream } from 'fs';
 import axios from 'axios';
 import { nanoid } from 'nanoid';
 import EventEmitter from 'events';
+
+import { exists } from './helpers.js';
 
 interface DownloadTask {
   id: string;
@@ -51,9 +54,27 @@ export class Downloader extends EventEmitter {
     });
     const totalLength = headers['content-length'];
     current.size = parseInt(totalLength);
-    const writer = fs.createWriteStream(
-      path.join(path.resolve('../models'), current.type, current.filename),
+
+    const filename = current.filename;
+    const partFilename = `${filename}.part`;
+
+    const filePath = path.join(
+      path.resolve('../models'),
+      current.type,
+      filename,
     );
+    const partPath = path.join(
+      path.resolve('../models'),
+      current.type,
+      partFilename,
+    );
+
+    if (await exists(partPath)) {
+      // TODO: Continue download?
+      await fs.unlink(partPath);
+    }
+
+    const writer = createWriteStream(partPath);
     const started_at = Date.now();
     this.emit('event', {
       event: 'download.start',
@@ -74,7 +95,9 @@ export class Downloader extends EventEmitter {
         },
       });
     });
-    data.on('end', () => {
+    data.on('end', async () => {
+      writer.close();
+      await fs.rename(partPath, filePath);
       this.emit('event', {
         event: 'download.end',
         data: {
