eadc7117310953472d6fbe127e60eec0f496fafc
Mat Sz <contact@matsz.dev>
2023-12-11 14:36:39 +0100

    add settings validation

--- api/src/index.ts
+++ api/src/index.ts
@@ -182,6 +182,70 @@ const downloadBody = {
   required: ['url', 'filename', 'type'],
 } as const satisfies JSONSchema;
 
+const promptBody = {
+  type: 'object',
+  properties: {
+    project_id: {
+      type: 'number',
+    },
+    models: {
+      type: 'object',
+      properties: {
+        base: {
+          type: 'object',
+          properties: { name: { type: 'string' } },
+          required: ['name'],
+        },
+        loras: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+              strength: { type: 'number' },
+            },
+            required: ['name', 'strength'],
+          },
+        },
+        controlnets: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+              strength: { type: 'number' },
+              image: { type: 'string' },
+            },
+            required: ['name', 'strength', 'image'],
+          },
+        },
+      },
+      required: ['base'],
+    },
+    conditioning: {
+      type: 'object',
+      properties: {
+        positive: { type: 'string' },
+        negative: { type: 'string' },
+      },
+      required: ['positive', 'negative'],
+    },
+    sampler: {
+      type: 'object',
+      properties: {
+        seed: { type: 'number' },
+        steps: { type: 'number' },
+        cfg: { type: 'number' },
+        denoise: { type: 'number' },
+        sampler: { type: 'string' },
+        scheduler: { type: 'string' },
+      },
+      required: ['seed', 'steps', 'cfg', 'denoise', 'sampler', 'scheduler'],
+    },
+  },
+  required: ['project_id', 'models', 'conditioning', 'sampler'],
+} as const satisfies JSONSchema;
+
 app.get('/info', async () => {
   return {
     samplers: comfy.samplers,
@@ -367,9 +431,21 @@ app.register(
 
 app.register(
   async fastify => {
-    fastify.post('/', async request => {
-      comfy.send('prompt', request.body);
-    });
+    fastify.post<{
+      Body: FromSchema<typeof promptBody>;
+    }>(
+      '/',
+      {
+        schema: {
+          body: promptBody,
+        },
+      },
+      async request => {
+        comfy.send('prompt', {
+          ...request.body,
+        });
+      },
+    );
   },
   { prefix: '/prompts' },
 );
