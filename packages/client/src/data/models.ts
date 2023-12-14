12c4bf42f6f7a116a51f5666a3967a178c30d0d0
Mat Sz <contact@matsz.dev>
2023-12-14 19:37:42 +0100

    comment out irrelevant models

--- packages/client/src/data/models.ts
+++ packages/client/src/data/models.ts
@@ -128,130 +128,130 @@ export const downloadable: DownloadableModelGroup[] = [
       },
     ],
   },
-  {
-    name: 'Stable Diffusion XL ReVision',
-    type: ModelType.CLIP_VISION,
-    description: 'Allows usage of images to prompt SDXL.',
-    models: [
-      {
-        name: 'CLIP Vision',
-        source: 'huggingface',
-        type: ModelType.CLIP_VISION,
-        downloads: [
-          {
-            type: ModelType.CLIP_VISION,
-            filename: 'sd_xl_revision.safetensors',
-            url: 'https://huggingface.co/comfyanonymous/clip_vision_g/resolve/main/clip_vision_g.safetensors',
-            size: 3689911098,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: 'TAESD',
-    type: ModelType.VAE_APPROX,
-    description:
-      'Allows steps to be previewed while the image is being generated.',
-    models: [
-      {
-        name: 'TAESD',
-        source: 'github',
-        type: ModelType.VAE_APPROX,
-        downloads: [
-          {
-            type: ModelType.VAE_APPROX,
-            filename: 'taesd_decoder.pth',
-            url: 'https://github.com/madebyollin/taesd/raw/main/taesd_decoder.pth',
-            size: 4915980,
-          },
-          {
-            type: ModelType.VAE_APPROX,
-            filename: 'taesd_encoder.pth',
-            url: 'https://github.com/madebyollin/taesd/raw/main/taesd_encoder.pth',
-            size: 4915788,
-          },
-          {
-            type: ModelType.VAE_APPROX,
-            filename: 'taesdxl_decoder.pth',
-            url: 'https://github.com/madebyollin/taesd/raw/main/taesdxl_decoder.pth',
-            size: 4915221,
-          },
-          {
-            type: ModelType.VAE_APPROX,
-            filename: 'taesdxl_encoder.pth',
-            url: 'https://github.com/madebyollin/taesd/raw/main/taesdxl_encoder.pth',
-            size: 4915029,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: 'RealESRGAN',
-    type: ModelType.UPSCALE_MODEL,
-    description: 'Allows for upscaling of output images.',
-    models: [
-      {
-        name: 'RealESRGAN x4 plus',
-        source: 'github',
-        type: ModelType.UPSCALE_MODEL,
-        recommended: true,
-        description: 'A better universal 4x upscale model.',
-        downloads: [
-          {
-            type: ModelType.UPSCALE_MODEL,
-            filename: 'realesrgan_x4plus.pth',
-            url: 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth',
-            size: 67040989,
-          },
-        ],
-      },
-      {
-        name: 'RealESRGAN x4 plus - Anime',
-        source: 'github',
-        type: ModelType.UPSCALE_MODEL,
-        recommended: true,
-        description: 'A 4x upscale model tailored for anime/cartoon art.',
-        downloads: [
-          {
-            type: ModelType.UPSCALE_MODEL,
-            filename: 'realesrgan_x4plus_anime.pth',
-            url: 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth',
-            size: 17938799,
-          },
-        ],
-      },
-      {
-        name: 'RealESRGAN x4',
-        source: 'huggingface',
-        type: ModelType.UPSCALE_MODEL,
-        description: 'An universal 4x upscale model.',
-        downloads: [
-          {
-            type: ModelType.UPSCALE_MODEL,
-            filename: 'realesrgan_x4.pth',
-            url: 'https://huggingface.co/sberbank-ai/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth',
-            size: 67040989,
-          },
-        ],
-      },
-      {
-        name: 'RealESRGAN x2',
-        source: 'huggingface',
-        type: ModelType.UPSCALE_MODEL,
-        description: 'An universal 2x upscale model.',
-        downloads: [
-          {
-            type: ModelType.UPSCALE_MODEL,
-            filename: 'realesrgan_x2.pth',
-            url: 'https://huggingface.co/sberbank-ai/Real-ESRGAN/resolve/main/RealESRGAN_x2.pth',
-            size: 67061725,
-          },
-        ],
-      },
-    ],
-  },
+  // {
+  //   name: 'Stable Diffusion XL ReVision',
+  //   type: ModelType.CLIP_VISION,
+  //   description: 'Allows usage of images to prompt SDXL.',
+  //   models: [
+  //     {
+  //       name: 'CLIP Vision',
+  //       source: 'huggingface',
+  //       type: ModelType.CLIP_VISION,
+  //       downloads: [
+  //         {
+  //           type: ModelType.CLIP_VISION,
+  //           filename: 'sd_xl_revision.safetensors',
+  //           url: 'https://huggingface.co/comfyanonymous/clip_vision_g/resolve/main/clip_vision_g.safetensors',
+  //           size: 3689911098,
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'TAESD',
+  //   type: ModelType.VAE_APPROX,
+  //   description:
+  //     'Allows steps to be previewed while the image is being generated.',
+  //   models: [
+  //     {
+  //       name: 'TAESD',
+  //       source: 'github',
+  //       type: ModelType.VAE_APPROX,
+  //       downloads: [
+  //         {
+  //           type: ModelType.VAE_APPROX,
+  //           filename: 'taesd_decoder.pth',
+  //           url: 'https://github.com/madebyollin/taesd/raw/main/taesd_decoder.pth',
+  //           size: 4915980,
+  //         },
+  //         {
+  //           type: ModelType.VAE_APPROX,
+  //           filename: 'taesd_encoder.pth',
+  //           url: 'https://github.com/madebyollin/taesd/raw/main/taesd_encoder.pth',
+  //           size: 4915788,
+  //         },
+  //         {
+  //           type: ModelType.VAE_APPROX,
+  //           filename: 'taesdxl_decoder.pth',
+  //           url: 'https://github.com/madebyollin/taesd/raw/main/taesdxl_decoder.pth',
+  //           size: 4915221,
+  //         },
+  //         {
+  //           type: ModelType.VAE_APPROX,
+  //           filename: 'taesdxl_encoder.pth',
+  //           url: 'https://github.com/madebyollin/taesd/raw/main/taesdxl_encoder.pth',
+  //           size: 4915029,
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'RealESRGAN',
+  //   type: ModelType.UPSCALE_MODEL,
+  //   description: 'Allows for upscaling of output images.',
+  //   models: [
+  //     {
+  //       name: 'RealESRGAN x4 plus',
+  //       source: 'github',
+  //       type: ModelType.UPSCALE_MODEL,
+  //       recommended: true,
+  //       description: 'A better universal 4x upscale model.',
+  //       downloads: [
+  //         {
+  //           type: ModelType.UPSCALE_MODEL,
+  //           filename: 'realesrgan_x4plus.pth',
+  //           url: 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth',
+  //           size: 67040989,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       name: 'RealESRGAN x4 plus - Anime',
+  //       source: 'github',
+  //       type: ModelType.UPSCALE_MODEL,
+  //       recommended: true,
+  //       description: 'A 4x upscale model tailored for anime/cartoon art.',
+  //       downloads: [
+  //         {
+  //           type: ModelType.UPSCALE_MODEL,
+  //           filename: 'realesrgan_x4plus_anime.pth',
+  //           url: 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth',
+  //           size: 17938799,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       name: 'RealESRGAN x4',
+  //       source: 'huggingface',
+  //       type: ModelType.UPSCALE_MODEL,
+  //       description: 'An universal 4x upscale model.',
+  //       downloads: [
+  //         {
+  //           type: ModelType.UPSCALE_MODEL,
+  //           filename: 'realesrgan_x4.pth',
+  //           url: 'https://huggingface.co/sberbank-ai/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth',
+  //           size: 67040989,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       name: 'RealESRGAN x2',
+  //       source: 'huggingface',
+  //       type: ModelType.UPSCALE_MODEL,
+  //       description: 'An universal 2x upscale model.',
+  //       downloads: [
+  //         {
+  //           type: ModelType.UPSCALE_MODEL,
+  //           filename: 'realesrgan_x2.pth',
+  //           url: 'https://huggingface.co/sberbank-ai/Real-ESRGAN/resolve/main/RealESRGAN_x2.pth',
+  //           size: 67061725,
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
   {
     name: 'QRCode ControlNet',
     type: ModelType.CONTROLNET,
