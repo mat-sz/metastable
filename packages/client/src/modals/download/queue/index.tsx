e201564403a89d8744b53296295cd4de3f64e1b9
Mat Sz <contact@matsz.dev>
2023-12-29 14:03:59 +0100

    improve download manager ux

--- packages/client/src/modals/download/queue/index.tsx
+++ packages/client/src/modals/download/queue/index.tsx
@@ -1,49 +1,54 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { BsX } from 'react-icons/bs';
 
 import styles from './index.module.scss';
 import { mainStore } from '../../../stores/MainStore';
-import { IconButton } from '../../../components';
 import { filesize } from '../../../helpers';
 
 export const Queue: React.FC = observer(() => {
   const downloads = mainStore.downloads;
   return (
     <div>
-      {downloads.queue.map(download => (
-        <div key={download.id} className={styles.item}>
-          <div>
-            <div>{download.name}</div>
+      {downloads.queue.map(download => {
+        const percent = (download.progress / download.size) * 100;
+        const speed =
+          download.startedAt && download.state === 'in_progress'
+            ? download.progress /
+              ((new Date().getTime() - download.startedAt) / 1000)
+            : undefined;
+
+        return (
+          <div key={download.id} className={styles.item}>
             <div>
-              <progress value={download.progress} max={download.size} />
+              <div>{download.name}</div>
             </div>
-          </div>
-          <div>
             <div>
-              {filesize(download.progress)} / {filesize(download.size)}
-            </div>
-            {download.startedAt && download.state === 'in_progress' && (
-              <div>
-                {filesize(
-                  download.progress /
-                    ((new Date().getTime() - download.startedAt) / 1000),
-                )}
-                /s
+              <div className={styles.progress}>
+                <div className={styles.progressInfo}>
+                  <span>{Math.round(percent)}%</span>
+                  {speed && <span>[{filesize(speed)}/s]</span>}
+                  <span>
+                    [{filesize(download.progress)} / {filesize(download.size)}]
+                  </span>
+                  <span>[{download.state}]</span>
+                </div>
+                <div
+                  className={styles.progressBar}
+                  style={{ width: `${percent}%` }}
+                ></div>
               </div>
-            )}
+            </div>
+            <div>
+              {(download.state === 'in_progress' ||
+                download.state === 'queued') && (
+                <button onClick={() => downloads.cancel(download.id)}>
+                  Cancel
+                </button>
+              )}
+            </div>
           </div>
-          <div>{download.state}</div>
-          <div>
-            <IconButton
-              onClick={() => downloads.cancel(download.id)}
-              title="Cancel"
-            >
-              <BsX />
-            </IconButton>
-          </div>
-        </div>
-      ))}
+        );
+      })}
       {downloads.queue.length === 0 && <div>No active downloads.</div>}
     </div>
   );
