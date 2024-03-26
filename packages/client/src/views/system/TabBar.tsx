6c2d33444df3626603daff0d044444fbd051c491
Mat Sz <contact@matsz.dev>
2024-03-26 19:47:16 +0100

    feat: display download progress on tab bar

--- packages/client/src/views/system/TabBar.tsx
+++ packages/client/src/views/system/TabBar.tsx
@@ -9,6 +9,7 @@ import { mainStore } from '$stores/MainStore';
 import type { BaseProject } from '$stores/project';
 import styles from './TabBar.module.scss';
 import { ProgressBar } from '$components/progressBar';
+import { TaskState } from '@metastable/types';
 
 const TAB_ITEM = 'project_tab';
 
@@ -83,23 +84,39 @@ export const ProjectTab: React.FC<{ project: BaseProject }> = observer(
   },
 );
 
-export const ViewTab: React.FC<React.PropsWithChildren<{ viewId: string }>> =
-  observer(({ viewId, children }) => {
-    return (
-      <div
-        className={clsx(styles.tab, {
-          [styles.selected]: mainStore.view === viewId,
-        })}
-        onClick={() =>
-          runInAction(() => {
-            mainStore.view = viewId;
-          })
-        }
-      >
-        <span>{children}</span>
-      </div>
-    );
-  });
+export const ViewTab: React.FC<
+  React.PropsWithChildren<{
+    viewId: string;
+    badge?: string | number;
+    value?: number;
+    max?: number;
+    marquee?: boolean;
+  }>
+> = observer(({ viewId, badge, value, max, marquee, children }) => {
+  return (
+    <div
+      className={clsx(styles.tab, {
+        [styles.selected]: mainStore.view === viewId,
+      })}
+      onClick={() =>
+        runInAction(() => {
+          mainStore.view = viewId;
+        })
+      }
+    >
+      {!!badge && <span className={styles.tabBadge}>{badge}</span>}
+      {!!(max || marquee) && (
+        <ProgressBar
+          className={styles.tabProgress}
+          value={value}
+          max={max}
+          marquee={marquee}
+        />
+      )}
+      <span>{children}</span>
+    </div>
+  );
+});
 
 export const TabBar: React.FC = observer(() => {
   const [, drop] = useDrop(
@@ -116,6 +133,22 @@ export const TabBar: React.FC = observer(() => {
     [],
   );
 
+  const downloads = mainStore.tasks.downloads;
+  const remaining = downloads.filter(
+    item =>
+      item.state === TaskState.RUNNING ||
+      item.state === TaskState.PREPARING ||
+      item.state === TaskState.QUEUED,
+  );
+  const downloadValue = remaining.reduce(
+    (total, item) => total + item.data.offset,
+    0,
+  );
+  const downloadMax = remaining.reduce(
+    (total, item) => total + item.data.size,
+    0,
+  );
+
   return (
     <div ref={drop} className={styles.tabs}>
       <div className={styles.logo}>Metastable</div>
@@ -125,7 +158,12 @@ export const TabBar: React.FC = observer(() => {
       <ViewTab viewId="settings">
         <BsGearFill />
       </ViewTab>
-      <ViewTab viewId="models">
+      <ViewTab
+        viewId="models"
+        badge={remaining.length}
+        value={downloadValue}
+        max={downloadMax}
+      >
         <BsBox />
       </ViewTab>
       {mainStore.projects.projects.map(project => (
