80f2213407044530973bfd722d0bdf3556fc5f8c
Mat Sz <contact@matsz.dev>
2024-11-05 19:35:08 +0100

    chore: fix unit tests

--- packages/metastable/vitest.config.ts
+++ packages/metastable/vitest.config.ts
@@ -1,8 +1,14 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     name: '@metastable/metastable',
     globals: true,
+  },
+  resolve: {
+    alias: {
+      '#metastable': path.resolve(__dirname, './src/metastable.ts'),
+    },
   },
 });
